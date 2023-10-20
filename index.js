4f2b7e354d8d864e7205f78c29b5271db58799a4
Shariar <shariarnafis86@gmail.com>
2023-10-20 19:26:53 +0600

    coplete data update method!

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -31,6 +31,12 @@ async function run() {
         const cursor = productCollection.find()
         const result = await cursor.toArray()
         res.send(result)
+    })
+    app.get('/brand/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await productCollection.findOne(query)
+      res.send(result)
     })
 
     app.post('/:brand', async(req, res)=>{
